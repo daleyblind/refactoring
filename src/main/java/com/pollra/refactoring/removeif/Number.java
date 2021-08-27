53a3e759794ab4b45fad3692282437ff26e6483c
tony <tony@niceday.io>
2021-08-28 07:44:50 +0900

    remove if toString 삭제

--- src/main/java/com/pollra/refactoring/removeif/Number.java
+++ src/main/java/com/pollra/refactoring/removeif/Number.java
@@ -30,11 +30,4 @@ public class Number {
     public int getNo() {
         return no;
     }
-
-    @Override
-    public String toString() {
-        return "Number{" +
-                "no=" + no +
-                "}";
-    }
 }
