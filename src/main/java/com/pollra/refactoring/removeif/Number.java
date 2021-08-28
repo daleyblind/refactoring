13e786812b42720eee912004e51f4d31a0a61a0f
tony <tony@niceday.io>
2021-08-28 20:33:25 +0900

    remove if 리팩토링 추가

--- src/main/java/com/pollra/refactoring/removeif/Number.java
+++ src/main/java/com/pollra/refactoring/removeif/Number.java
@@ -7,21 +7,23 @@ import java.util.Optional;
 /**
  * @since       2021.08.27
  * @author      pollra
- * @description number
  **********************************************************************************************************************/
 public class Number {
-    private int no;
+    private final int no;
+    private final static Map<String, BinaryFunction<Integer, Number>> operatorMap;
+    static {
+        operatorMap = new HashMap<>();
+        operatorMap.put("+", (a, b) -> new Number(a + b));
+        operatorMap.put("-", (a, b) -> new Number(a - b));
+        operatorMap.put("*", (a, b) -> new Number(a * b));
+        operatorMap.put("/", (a, b) -> new Number(a / b));
+    }
 
     public Number(int no) {
         this.no = no;
     }
 
     public Number calculate(String expression, Number number) {
-        Map<String, BinaryFunction<Integer, Number>> operatorMap = new HashMap<>();
-        operatorMap.put("+", (a, b) -> new Number(a + b));
-        operatorMap.put("-", (a, b) -> new Number(a - b));
-        operatorMap.put("*", (a, b) -> new Number(a * b));
-        operatorMap.put("/", (a, b) -> new Number(a / b));
         return Optional.ofNullable(operatorMap.get(expression))
                 .orElseThrow(IllegalArgumentException::new)
                 .apply(this.no, number.no);
